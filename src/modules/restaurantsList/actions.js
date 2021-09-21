b2b3f9f7fe3cf1405e64cb52bcd6b5c941db3e38
Fabio Mendoza Bello <fabiomb@ext.inditex.com>
2021-09-21 10:32:35 +0100

    Creado mock-service

--- src/modules/restaurantsList/actions.js
+++ src/modules/restaurantsList/actions.js
@@ -1,10 +1,15 @@
 import menus from "./reutaurantsMenus.json";
 
 export const getMenus = () => {
-    return new Promise((resolve,reject) => {
+/*    return new Promise((resolve,reject) => {
         setTimeout(() => {
             resolve(menus);
         },3000);
+    });*/
+
+    console.log("get menus");
+
+    fetch(`${Config.backendBaseUrl}/menus`).then(response => {
+        debugger;
     });
-    
 }
