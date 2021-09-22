26411f2f48a8417844854d0b4868f78116216384
Fabio Mendoza Bello <fabiomb@ext.inditex.com>
2021-09-22 12:59:49 +0100

    redux

--- src/modules/restaurantsList/actions.js
+++ src/modules/restaurantsList/actions.js
@@ -1,8 +1,29 @@
 import { Config } from "../../config/Config";
 
-export const getMenus = () => {
-    console.log("Get menus");
+export const USER_MENUS_REQUEST = 'GET_MENUS_REQUEST';
+export const USER_MENUS_RESPONSE = 'GET_MENUS_RESPONSE';
 
-    return fetch(`${Config.backendBaseUrl}/menus`)
-        .then(response => response.json());
+export const getMenus = () => {
+    return dispatch => {
+        dispatch({
+          type: USER_MENUS_REQUEST
+        });
+
+        // Indicar a Redux que estamos cargando
+        return fetch(Config.backendBaseUrl + '/menus', {
+            method : 'post',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body : JSON.stringify({
+                login,
+                password
+            })
+        }).then(response  => response.json()).then(userInfo => {
+            dispatch({
+                type: USER_MENUS_RESPONSE,
+                userInfo
+            });
+        });
+    }
 }
